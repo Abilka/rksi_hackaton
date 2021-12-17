2524624e967cc5949c2a3d90241b73b6e7762f39
1 <1>
2021-12-18 01:10:16 +0400

    Добавлена выгрузка преподов из базы и выборка отсутсвующих преподов из нее.

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,27 @@
+import json
+import typing
+
 import pandas
 
 import database
 
-con = database.DbSchedule().connection
 
-print(pandas.read_sql('SELECT * FROM schedule', con)
-      )
+class Calculate:
+    def __init__(self):
+        self._db_data: pandas.DataFrame = pandas.read_sql('SELECT * FROM schedule', database.DbSchedule().connection)
+        self._missed: pandas.DataFrame = pandas.read_excel('Отсутствующие.xlsx')
+
+    def miss_teacher(self) -> pandas.DataFrame:
+        """Возвращает DataFrame с расписание отсутсвующих преподователей"""
+        teacher_apair: pandas.DataFrame = self._db_data[self._db_data['is_group'] == 0]
+        self._missed: typing.List = list(map(lambda x: x.upper(), list(self._missed['ФИО'].values)))
+        return teacher_apair.loc[teacher_apair['group_name'].isin(self._missed)]
+
+    def need_change(self):
+        """Возвращает пары которые надо заменить"""
+        json_data = self.miss_teacher()
+
+        json.loads(json_data.iloc[0]['json_data'])
+        print(json_data)
+
+Calculate().need_change()
