f0d533819b3210b8a75e4dcd5246ab9d8226520d
1 <1>
2021-12-18 20:35:33 +0400

    Написано немного сервера и взаимодействие с бд

--- database.py
+++ database.py
@@ -2,6 +2,75 @@ import json
 import sqlite3
 from typing import List
 
+import mysql.connector
+import setting
+import hashlib
+import requests
+
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+        self.host = "http://{0}:{1}".format(setting.SERVER_IP, setting.SERVER_PORT)
+        self.auth = self.is_login
+
+    @property
+    def is_login(self):
+        if requests.get('{0}/login'.format(self.host),
+                        params={'login':self.login, 'password': self.coding_password(self.password)}).json()['result'] is True:
+            return True
+        return False
+
+    def set_password(self, new_password: str) -> None:
+        if self.auth is True:
+            requests.get('{0}/new_password'.format(self.host),
+                         params={'login':self.login, 'password': self.coding_password(new_password)}).json()
+
+    @staticmethod
+    def coding_password(password: str) -> str:
+        return hashlib.sha512(password.encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def get_user() -> dict:
+        return requests.get('http://{0}:{1}/get_user'.format(setting.SERVER_IP, setting.SERVER_PORT)).json()
+
+class Db:
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+            host="192.168.1.67",
+            user="root",
+            password="",
+            database='user'
+        )
+        self.cursor = self.mydb.cursor()
+
+    def is_login(self, login: str, password: str) -> bool:
+        self.cursor.execute('SELECT password FROM users WHERE login = %s AND password = %s', (login, password))
+        if self.cursor.fetchone() is not None:
+            return True
+        return False
+
+    def new_password(self, login: str, password: str) -> None:
+        self.cursor.execute('UPDATE `users` SET `password`=%s WHERE login=%s', (password, login))
+        self.mydb.commit()
+
+    def is_registerd(self, login):
+        self.cursor.execute('SELECT password FROM users WHERE login = %s', (login,))
+        if self.cursor.fetchone() is not None:
+            return True
+        return False
+
+    def new_user(self, login: str, hash_password: str, role: str) -> bool:
+        if self.is_registerd(login) is False:
+            self.cursor.execute('INSERT INTO `users`(`login`, `password`, `role`) VALUES (%s,%s,%s)',
+                                (login, hash_password, role))
+            self.mydb.commit()
+            return True
+        return False
+
+    def get_users(self):
+        self.cursor.execute('SELECT login, role FROM users')
+        return self.cursor.fetchall()
 
 class DbSchedule:
     def __init__(self) -> None:
--- server.py
+++ server.py
@@ -2,13 +2,15 @@
 import json
 
 import flask
+from flask import request
 
+import database
 import scheduler
 import setting
 
-
 app = flask.Flask(__name__)
 schedule = scheduler.Schedule()
+
 
 @app.route('/create_changed', methods=['GET'])
 def create_changed():
@@ -17,13 +19,34 @@ def create_changed():
     with open("Лист замен.json", 'r') as f:
         return json.dumps(f.read())
 
+
 @app.route('/take_changed', methods=['GET'])
 def send_changed():
-
     if schedule.changed is None:
         schedule.changed_needed()
     schedule.save_json()
     with open("Лист замен.json", 'r') as f:
         return json.dumps(f.read())
 
-app.run(port=setting.SERVER_PORT, host="0.0.0.0")
+
+@app.route('/login', methods=['GET'])
+def login():
+    return {"result": database.Db().is_login(request.args['login'], request.args['password'])}
+
+
+@app.route('/new_password', methods=['GET'])
+def new_password():
+    return {"result": database.Db().new_password(request.args['login'], request.args['password'])}
+
+@app.route('/'
+           '', methods=['GET'])
+def new_user():
+    return {"result": database.Db().new_user(request.args['login'], request.args['password'], request.args['role'])}
+
+@app.route('/get_user', methods=['GET'])
+def get_user():
+    return {"result": database.Db().get_users()}
+
+
+# app.run(port=setting.SERVER_PORT, host="0.0.0.0")
+app.run(port=setting.SERVER_PORT)
